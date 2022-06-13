2faf37230a5edfd59962130ce3b889bb269b33ad
JonathanNoH <jonathan897h@gmail.com>
2022-06-13 13:29:18 -0600

    add test to shop for add to cart button

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 const Shop = () => {
+  const [cart, setCart] = useState({});
+
+  const addItemToCart = (item, currentAmount) => {
+    setCart(prevState => {
+      return { ...prevState, [item]: (currentAmount + 1)}
+    })
+  }
 
   return (
     <div>
       <h1>Shop</h1>
-      <Item title="Apple" description="fruit"/>
+      <Item 
+        title="Apple" 
+        description="fruit" 
+        amount={('Apple' in cart) ? cart.Apple : 0 }
+        onAdd={addItemToCart}
+      />
     </div>
   )
 }
--- src/Tests/Shop.test.js
+++ src/Tests/Shop.test.js
@@ -3,6 +3,17 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";  // optional
 import userEvent from "@testing-library/user-event";
 import Shop from "../Components/Shop";
+/*
+jest.mock("./Item", () => ({title, description, amount, onAdd}) => {
+  const clickHandler = () => onAdd(title, amount);
+  <div>
+    <h3>{title || "Missing Title"}</h3>
+    <div role={title + "-description"}>{description || "Missing Description"}</div>
+    <button onClick={clickHandler} aria-label={`Add ${title} To Cart`}>Add To Cart</button>
+    <div role={title + "-in-cart"}>{amount}</div>
+  </div>
+});
+*/
 
 describe('Shop Component', () => {
   it("renders shop", () => {
@@ -10,3 +21,16 @@ describe('Shop Component', () => {
     expect(container).toMatchSnapshot();
   })
 });
+
+describe('Shop Logic', () => {
+  it("adds item to cart", () => {
+    render(<Shop />);
+    const button = screen.getByRole('button', { name: "Add Apple To Cart" });
+    const amount = screen.getByRole('Apple-in-cart');
+    expect(amount.textContent).toBe("0");
+
+    userEvent.click(button);
+
+    expect(amount.textContent).toBe("1");
+  })
+})
