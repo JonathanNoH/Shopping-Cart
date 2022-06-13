3b6936bdf003eaf4af48f5f0818957a6d72c5c02
JonathanNoH <jonathan897h@gmail.com>
2022-06-13 13:02:13 -0600

    update cart aria label

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Item = ({title, description, amount, onAdd}) => {
-  const clickHandler = (e) => onAdd(e.target.value);
+  const clickHandler = () => onAdd(title, amount);
 
   return (
     <div>
       <h3>{title || "Missing Title"}</h3>
       <div role={title + "-description"}>{description || "Missing Description"}</div>
-      <button onClick={clickHandler}>Add To Cart</button>
+      <button onClick={clickHandler} aria-label={`Add ${title} To Cart`}>Add To Cart</button>
       <div role={title + "-in-cart"}>{amount}</div>
     </div>
   )
